466669784d86ef79b2f155cce92aa441a0078aa6
OlgaMazura <ofmazura@gmail.com>
2023-01-30 19:06:55 +0100

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача №12. Работа в группах
+// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+// ● 34, 5 -> не кратно, остаток 4
+// ● 16, 4 -> кратно
+
+Console.Clear();
+Console.WriteLine("Enter number 1: ");
+int n1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number 2: ");
+int n2 = int.Parse(Console.ReadLine()!);
+
+if (n2 % n1 == 0)
+{
+    Console.WriteLine("YES!");
+}
+else
+{
+    Console.WriteLine("NO!");
+    int rest = n2 % n1;
+    Console.WriteLine($"The rest is {rest}");
+}
