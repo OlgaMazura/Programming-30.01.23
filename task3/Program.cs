ebbf3b7c6576ffe87cabdb888cd389a7cac821ce
OlgaMazura <ofmazura@gmail.com>
2023-01-30 19:37:25 +0100

    task4

--- task3/Program.cs
+++ task3/Program.cs
@@ -11,23 +11,17 @@ Console.WriteLine("Enter number 1: ");
 int n1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Enter number 2: ");
 int n2 = int.Parse(Console.ReadLine()!);
+int rest = n2 % n1;
 
-if (n2 % n1 == 0)
+if (rest == 0)
 {
     Console.WriteLine("YES!");
 }
 else
 {
     Console.WriteLine("NO!");
-    int rest = n2 % n1;
     Console.WriteLine($"The rest is {rest}");
 }
-// // Напишите программу, которая будет принимать на
-// // вход два числа и выводить, является ли второе число
-// // кратным первому. Если число 2 не кратно числу 1, то
-// // программа выводит остаток от деления.
-// // ● 34, 5 -> не кратно, остаток 4
-// // ● 16, 4 -> кратно
 
 // Console.Clear();
 // Console.WriteLine("Enter Number1: ");
---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// ● 14 -> нет
+// ● 46 -> нет
+// ● 161 -> да
+
+Console.Clear();
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n % 7 == 0 && n % 23 == 0)
+{
+    Console.WriteLine("YES!");
+}
+else
+{
+    Console.WriteLine("NO!");
+}
