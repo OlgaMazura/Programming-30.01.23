c264b9f062c75982f8bcea078efe80a4e2431e9e
OlgaMazura <ofmazura@gmail.com>
2023-01-30 20:31:15 +0100

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Enter number from 1 to 7: ");
+int DAY = int.Parse(Console.ReadLine()!);
+
+if(DAY==6 || DAY==7)
+{
+    Console.WriteLine($"YES!");
+}
+else
+{
+    Console.WriteLine($"NO!");
+}
