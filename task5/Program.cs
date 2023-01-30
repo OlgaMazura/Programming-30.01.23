2941580bd53fb3511db441eb5fd8562704edf195
OlgaMazura <ofmazura@gmail.com>
2023-01-30 19:52:48 +0100

    task5 added

--- task5/Program.cs
+++ task5/Program.cs
@@ -14,3 +14,20 @@ else
 {
     Console.WriteLine($"NO!");
 }
+
+// или такой вариант: Console.Clear();
+
+// Console.Write("Введите число a ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите число b ");
+// int b = int.Parse(Console.ReadLine()!);
+
+//  if (b * b == a || a * a == b)
+// {
+//     Console.Write("Да");
+// }
+// else
+// {
+//     Console.Write("Нет");
+// }
