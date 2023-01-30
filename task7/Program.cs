5908c90f2d5708ecc5e29f35ecb64a9f45f69aa1
OlgaMazura <ofmazura@gmail.com>
2023-01-30 20:59:59 +0100

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Enter three-digit number: ");
+int n = int.Parse(Console.ReadLine());
+
+int n1 = n % 100;
+int n2 = n % 10;
+int result = (n1 - n2)/10;
+
+Console.Write($"{result}"!);
