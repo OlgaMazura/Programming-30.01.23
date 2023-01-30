48d478fd21d35b175dc1e281dc98686df75aa56e
OlgaMazura <ofmazura@gmail.com>
2023-01-30 22:56:06 +0100

    task3

---
+++ task8/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine());
+int k = 10;
+int result = n % k;
+
+if (n < 99)
+{
+    Console.WriteLine($"There is no third number"!);
+}
+else
+{
+    Console.WriteLine($"{result}"!);
+}
+
+
+// int index = 0;
+
+// while (index < n)
+// {
+
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//     }
+
+//     index++;
+// }
