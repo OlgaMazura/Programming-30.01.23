a807950d758ed8572c07b61b8f04cbd2226a04fa
OlgaMazura <ofmazura@gmail.com>
2023-01-30 23:35:23 +0100

    task8 - example

--- task8/Program.cs
+++ task8/Program.cs
@@ -27,3 +27,11 @@ else
 
 //     index++;
 // }
+            // // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+            // Console.Write("Введите любое число: ");
+            // num = Convert.ToInt32(Console.ReadLine());
+            // string numStr = Convert.ToString(num);
+            // numDigit = numStr.Length;
+            // if (numDigit > 2) Console.WriteLine($"Третья цифра введённого числа: {numStr[2]}");
+            // else Console.WriteLine("третьей цифры нет");
+            // break;
