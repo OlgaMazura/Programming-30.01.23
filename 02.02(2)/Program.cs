bbb13c919327c19d7e9a890d78abfddc0e1a2a41
OlgaMazura <ofmazura@gmail.com>
2023-02-02 18:43:37 +0100

    02.02(1) & 02.02(2)

---
+++ 02.02(2)/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат 
+// точек в этой четверти (x и y).
+Console.Clear();
+Console.WriteLine("ВВедите № четверти: ");
+int N = int.Parse(Console.ReadLine()!);
+
+if (N == 1)
+{
+    Console.Write("Координаты Х > 0, Y > 0");
+}
+else if (N == 2)
+{
+    Console.Write("Координаты Х < 0, Y > 0");
+}
+else if (N == 3)
+{
+    Console.Write("Координаты Х < 0, Y < 0");
+}
+else if (N == 4)
+{
+    Console.Write("Координаты Х > 0, Y < 0");
+}
+else
+{
+    Console.Write("Введите значение от 1 до 4");
+}
