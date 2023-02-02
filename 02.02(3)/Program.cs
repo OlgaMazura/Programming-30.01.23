06f775f8a4c205f904eb64ced705420d4c5b2c0a
OlgaMazura <ofmazura@gmail.com>
2023-02-02 19:52:09 +0100

    02.02(3) & 02.02(4)

---
+++ 02.02(3)/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+//  • A (3,6); B (2,1) -> 5,09 
+//  • A (7,-5); B (1,-1) -> 7,21
+Console.Clear();
+Console.WriteLine("Введите X1: ");
+int X1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите X2: ");
+int X2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите Y1: ");
+int Y1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите Y2: ");
+int Y2 = int.Parse(Console.ReadLine()!);
+double X = Math.Pow(X2 - X1, 2);
+double Y = Math.Pow(Y2 - Y1, 2);
+
+double RESULT = Math.Sqrt(X + Y);
+Console.WriteLine(Math.Round(RESULT, 2));
+// или Console.WriteLine($"{RESULT:f2}!);
---
+++ 02.02(4)/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int I = 1;
+
+while (I <= N)
+{
+    Console.Write($"{Math.Pow(I,2)}");
+    if (I != N)
+    {
+        Console.Write(", ");
+    }
+    I++;
+}
