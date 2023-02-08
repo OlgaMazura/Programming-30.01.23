d619166fe0ec6a4407387e90d97a6a499aec8ef1
OlgaMazura <ofmazura@gmail.com>
2023-02-08 22:47:35 +0100

    08.02(5)

---
+++ 08.02(5)/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵) ; 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Enter number 1: ");
+int N1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number 2: ");
+int N2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{Math.Pow(N1, N2)}"!);
