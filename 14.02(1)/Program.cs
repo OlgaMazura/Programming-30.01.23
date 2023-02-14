1140405b3e30da3aac07291c8be25f76c767f1a8
OlgaMazura <ofmazura@gmail.com>
2023-02-14 20:49:53 +0100

    14.02(1)

---
+++ 14.02(1)/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+Console.Write("How many numbers do you want to enter? - ");
+int size = int.Parse(Console.ReadLine()!);
+Console.Write("Enter numbers separated by space - ");
+String[] NewArray = Console.ReadLine().Split();
+Console.WriteLine(String.Join(", ", NewArray));
+
+int CountPositiveElements(int[] NewArray)
+{
+    int Count = 0;
+    foreach (int i in NewArray)
+    {
+      if (i > 0)
+      {
+        Count++;
+      }  
+    }
+    return Count;
+}
+
+int N = CountPositiveElements(NewArray);
+Console.WriteLine($"There are {N} positive numbers"!);
