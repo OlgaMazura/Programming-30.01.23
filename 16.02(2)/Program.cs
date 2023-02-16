06e806631ebd7ea94c814d2f42f22a390f7ad003
OlgaMazura <ofmazura@gmail.com>
2023-02-16 22:06:35 +0100

    edition

---
+++ 16.02(2)/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. 
+// Выведите полученный массив на экран
+
+
+int[,] GetArray(int row, int col)
+{
+    int[,] Array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Array[i, j] = i + j;
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, col);
+PrintArray(array);
---
+++ 16.02(5)/Program.cs
@@ -0,0 +1,31 @@
+﻿// See Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Clear();
+
+double[,] GetDoubleArray(int m, int n, double MinValue, double MaxValue)
+{
+    double[,] Result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Result[i, j] = new Random().NextDouble() * (MaxValue - MinValue) + MinValue;
+        }
+    }
+    return Result;
+}
+
+void PrintArray(double[,] NewArray)
+{
+    for (int i = 0; i < NewArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < NewArray.GetLength(1); j++)
+        {
+            Console.Write($"{NewArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array2D = GetDoubleArray(3, 3, 1, 10);
+PrintArray(array2D);
